1ab049b458c59f948dcb365e7847ec80fe3a4006
AliAbdurohman16 <alyabdurohman16@gmail.com>
2024-07-10 14:25:09 +0700

    add view detail

--- app/src/main/java/com/example/petshop/Adapter/BestSellerAdapter.kt
+++ app/src/main/java/com/example/petshop/Adapter/BestSellerAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.petshop.Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.request.RequestOptions
+import com.example.petshop.Activity.DetailActivity
 import com.example.petshop.Model.ItemsModel
 import com.example.petshop.databinding.ViewholderBestSellerBinding
 
@@ -38,6 +40,12 @@ class BestSellerAdapter(val items: MutableList<ItemsModel>) :
             .load(items[position].picUrl[0])
             .apply(requestOption)
             .into(holder.binding.picBestSeller)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
+            intent.putExtra("object", items[position])
+            holder.itemView.context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int = items.size
---
+++ app/src/main/java/com/example/petshop/Helper/ChangeNumberItemsListener.kt
@@ -0,0 +1,5 @@
+package com.example.petshop.Helper
+
+interface ChangeNumberItemsListener {
+    fun onChanged()
+}
